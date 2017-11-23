66bc45b498824970c6a51182643b6bc53099b521
DESKTOP-QON2GQP\tuantran <vantuanbk95@gmail.com>
2017-11-23 07:40:22 +0700

    init chat

---
+++ UserMannagementServer/src/main/java/com/mycompany/usermanagementserver/serversocket/EventChatLauncher.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.usermanagementserver.serversocket;
+
+import com.corundumstudio.socketio.AckRequest; 
+import com.corundumstudio.socketio.Configuration; 
+import com.corundumstudio.socketio.SocketIOClient; 
+import com.corundumstudio.socketio.SocketIOServer; 
+import com.mycompany.usermanagementserver.serversocket.entity.Event;
+import io.socket.client.Socket;
+import org.apache.tomcat.util.net.SocketEvent;
+
+/**
+ *
+ * @author tuantran
+ */
+public class EventChatLauncher {
+    
+    public static void start() throws InterruptedException { 
+ 
+        Configuration config = new Configuration(); 
+        config.setHostname("localhost"); 
+        config.setPort(9092); 
+ 
+        final SocketIOServer server = new SocketIOServer(config); 
+        server.addEventListener("connect", Object.class, ((SocketIOClient socketIOClient, Object data, AckRequest ackRequest) -> {
+            System.out.println("connect");
+        }));
+        server.addEventListener("connected", Object.class, ((SocketIOClient socketIOClient, Object data, AckRequest ackRequest) -> {
+            System.out.println("connected");
+        }));
+        server.addEventListener("connection", Object.class, ((SocketIOClient socketIOClient, Object data, AckRequest ackRequest) -> {
+            System.out.println("connection");
+        }));
+        server.addEventListener(Event.CHAT, String.class, (SocketIOClient socketIOClient, String data, AckRequest ackRequest) -> {
+                System.out.println("data" + data);
+                socketIOClient.sendEvent(Event.CHAT, "heello");
+        }); 
+ 
+        server.start(); 
+ 
+        Thread.sleep(Integer.MAX_VALUE); 
+ 
+        server.stop(); 
+    } 
+    
+    public static void main(String[] srgs) {
+        try {
+            start();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ UserMannagementServer/src/main/java/com/mycompany/usermanagementserver/serversocket/entity/message/Message.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.usermanagementserver.serversocket.entity.message;
+
+/**
+ *
+ * @author tuantran
+ */
+public class Message {
+    
+    private String id;
+    private String fromUserId;
+    private String toUserId;
+    private String time;
+    private String readTime;
+    private String messageType;
+    private String value;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFromUserId() {
+        return fromUserId;
+    }
+
+    public void setFromUserId(String fromUserId) {
+        this.fromUserId = fromUserId;
+    }
+
+    public String getToUserId() {
+        return toUserId;
+    }
+
+    public void setToUserId(String toUserId) {
+        this.toUserId = toUserId;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getReadTime() {
+        return readTime;
+    }
+
+    public void setReadTime(String readTime) {
+        this.readTime = readTime;
+    }
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    
+}
